429b23ad2cf655a8efc03a978041c904789dfa69
Sushant Singh <sushant22007@gmail.com>
2026-01-09 15:21:19 +0530

    add initial App component with counter and input handling

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [name, setName] = useState("")
+
+  const [details, setDetails] = useState({age: 0, city: ""})
+
+  function increaseCount(){
+    setCounter(counter+1)
+  }
+
+  function handleInputChange(event){
+    setName(event.target.value)
+  }
+
+  console.log(counter)
+
+  return (
+    <div>
+      <input type="text" value={name} onChange={handleInputChange} placeholder='name' />
+      <input type='number' value={details.age} onChange={(e) => setDetails({...details, age: e.target.value})} placeholder='Age' />
+      <input type='text' value={details.city} onChange={(e) => setDetails({...details, city: e.target.value})} placeholder='City' />
+      <div> {name} of the student and {details.age} years old and lives in {details.city} and {counter} times </div>
+      <button onClick={increaseCount}> Increase </button>
+    </div>
+  )
+}
+
+export default App
